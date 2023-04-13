5c5b00d860da634a5fd4f94b493fe572bd9c860d
Altyn <deeze.nuts.fire@gmail.com>
2023-04-13 15:43:47 +0600

    made several students, wake up function works, main.py userevent works, count of sleeping student works

--- main.py
+++ main.py
@@ -1,42 +1,93 @@
 import pygame
 from student import Student
+
 pygame.init()
 
-bounds = (640, 360)
+
+bounds = (1280, 720)
 window = pygame.display.set_mode(bounds)
 pygame.display.set_caption("Wake up, pp2ease!")
 clock = pygame.time.Clock()
 grid = pygame.image.load(r"assets/grid.png")
 grid_rect = grid.get_rect()
+bubble_text = pygame.image.load(r"assets/bubble.png")
+
+font = pygame.font.Font(r"assets\dogicapixel.ttf")
+
 
 BLUE = (70,130,180)
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 
-(st_x, st_y) = (100, 250)
+(st_x, st_y) = (100, 300)
+(st2_x, st2_y) = (600, 300)
+temp = (st_x, st_y)
+temp2 = (st2_x, st2_y)
 (st_w, st_h) = (32, 32) # width and height of student sprite
-S1 = Student(st_x, st_y)
-# student_sprites = pygame.sprite.Group()
-# student_sprites.add(S1)
+
+student_sprites = pygame.sprite.Group()
+awake_set = set(())
+
+for i in range(4):
+    for j in range(6):
+        temp = (temp[0] + 70, temp[1])
+        id = '0' + str(i) + str(j)
+        student_sprites.add(Student(temp[0], temp[1], int(id)))
+        awake_set.add(int(id))
+    temp = (st_x, temp[1] + 100)
+
+for i in range(4):
+    for j in range(6):
+        temp2 = (temp2[0] + 70, temp2[1])
+        id = '1' + str(i) + str(j)
+        student_sprites.add(Student(temp2[0], temp2[1], int(id)))
+        awake_set.add(int(id))
+    temp2 = (st2_x, temp2[1] + 100)
+
 
 (mouse_x, mouse_y) = pygame.mouse.get_pos()
 
+next_level = pygame.USEREVENT + 1
+
+pygame.time.set_timer(next_level, 5000)
+
+
 while True:
+
+    
+
+    (b_x, b_y) = (1000, 1000)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
+        
+        if event.type == next_level:
+            for entity in student_sprites:
+                Student.change_speed(entity)
+                print(entity.a, entity.b)
+
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             (mouse_x, mouse_y) = pygame.mouse.get_pos()
-            # reset student's energy when clicked at asleep status
-            if (mouse_x <= (st_x + st_w) and mouse_y <= (st_y + st_h) and mouse_x >= (st_x - st_w) and mouse_y >= (st_y - st_h)) and  Student.get_status(S1) == 'asleep':
-                Student.energy_reset(S1)
-                print(f'{mouse_x, mouse_y}')
+            for entity in student_sprites:
+                Student.click_event(entity, mouse_x, mouse_y)
+                (b_x, b_y) = (50, 50)
+        
+            
+
     # window.blit(grid, grid_rect)
     window.fill(BLACK)
-    window.blit(S1.image, S1.rect)
+    window.blit(bubble_text, (b_x, b_y))
 
-    Student.energy_drain(S1)
+    # window.blit(bubble_text, (100,100))
+    for entity in student_sprites:
+        window.blit(entity.image, entity.rect)
+        Student.energy_drain(entity)
+        if entity.status == 'asleep' and entity.id in awake_set:
+            awake_set.remove(entity.id)
+        elif entity.status == 'awake' and entity.id not in awake_set:
+            awake_set.add(entity.id)
     
+    print(len(awake_set))
 
     pygame.display.update()
     clock.tick(60)
--- student.py
+++ student.py
@@ -1,22 +1,30 @@
 import pygame
 from random import randint
+pygame.init()
+
+fx = pygame.mixer.Sound(r"assets/fx.mp3")
 
 class Student(pygame.sprite.Sprite):
-    def __init__(self, x, y): # (x, y) - coordinates where student is sitting
+    def __init__(self, x, y, id): # (x, y) - coordinates where student is sitting
         super().__init__()
         self.status = 'awake'
-
+        self.id = id
         self.image = pygame.image.load(r"assets\s1.png")
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
+        self.x = x
+        self.y = y
+        self.w = 32
+        self.h = 32
 
-        self.a = 5000 # energy looses every 5 seconds
-        self.b = 10000 # every 10 seconds
+        self.a = 2000 # energy looses every 5 seconds
+        self.b = 5000 # every 10 seconds
         self.energy_speed = randint(self.a, self.b)
-        self.energy = 100 
+        self.energy = 125 
         # everyone has 100 % energy in the beginning
 
         self.next_time = 0
+        
     
     def energy_drain(self):
         current_time = pygame.time.get_ticks()
@@ -33,14 +41,28 @@ class Student(pygame.sprite.Sprite):
         else:
             self.set_asleep()
             self.status = 'asleep'
-        print(f'energy: {self.energy}')
-    
-    def energy_reset(self):
-        self.energy = 125
-    
+        # print(f'energy: {self.energy}')
+
+    def click_event(self, m_x, m_y):
+            # reset student's energy when clicked at asleep status
+        if (m_x <= (self.x + self.w) and m_y <= (self.y + self.h) and m_x >= (self.x - self.w) and m_y >= (self.y - self.h)) and self.status == 'asleep':
+            self.energy = 125
+            print(f'{m_x, m_y}')
+            fx.play()
+        
+
+            
+        
     def get_status(self):
         return self.status
+    def get_id(self):
+        return self.id
     
+    def change_speed(self):
+        if self.a > 0 and self.b > 0:
+            self.a -= 500
+            self.b -=500
+
     def set_awake(self):
         self.image = pygame.image.load(r"assets\s1.png")
     
